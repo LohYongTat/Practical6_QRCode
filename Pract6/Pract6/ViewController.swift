60c2577020335b96d46dc89928fdcac27a5fb383
Practical 2 <s10169704@connect.np.edu.sg>
2020-01-07 02:47:52 -0800

    mm

--- Pract6/Pract6/ViewController.swift
+++ Pract6/Pract6/ViewController.swift
@@ -10,18 +10,45 @@ import UIKit
 import QRCode
 
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var txtValue: UITextField!
+    @IBOutlet weak var imageViewQR: UIImageView!
+    @IBOutlet weak var Button1: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
-        let qrcode = QRCode("Hello World!!")
-        imageView.image = qrcode!.image
+        /*let qrcode = QRCode("Hello World!!")
+        imageView.image = qrcode!.image*/
+     txtValue.resignFirstResponder()
+      print(txtValue.text!)
+      var qrcode = QRCode(txtValue.text!)
+      qrcode?.color = CIColor(rgba: "88000")
+      qrcode?.backgroundColor = CIColor(rgba: "ffffff")
+      imageViewQR.image = qrcode?.image
+     
+      
+        }
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        txtValue.resignFirstResponder()
+        print(txtValue.text!)
+        var qrcode = QRCode(txtValue.text!)
+        qrcode?.color = CIColor(rgba: "88000")
+        qrcode?.backgroundColor = CIColor(rgba: "ffffff")
+        imageViewQR.image = qrcode?.image
+       
         
+        return true
     }
-
+    @IBAction func button(_sender:Any)
+    {
+    txtValue.resignFirstResponder()
+    print(txtValue.text!)
+    var qrcode = QRCode(txtValue.text!)
+    qrcode?.color = CIColor(rgba: "88000")
+    qrcode?.backgroundColor = CIColor(rgba: "ffffff")
+    imageViewQR.image = qrcode?.image    }
 }
 
